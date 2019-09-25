daf5f7cab6a7b2f5dca416e7dd07e350b6bad851
hdx <sanjayheaven.com>
2019-09-25 19:05:30 +0800

    rm dir setting

--- manage.py
+++ manage.py
@@ -5,9 +5,9 @@ import sys
 
 
 def main():
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE','hdxsite.settings')
-    profile = os.environ.get('HDX_DISITE_PROFILE','develop')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hdxsite.setting.{}'.format(profile))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','hdxsite.settings')
+    # profile = os.environ.get('HDX_DISITE_PROFILE','develop')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hdxsite.setting.{}'.format(profile))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
